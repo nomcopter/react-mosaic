94886ae28416b2ef8ac4971ae530e751cb489c1b
Kevin Verdieck <kverdieck@palantir.com>
2017-03-31 11:37:20 -0700

    Upgrade blueprint. Fixes #7

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -5,9 +5,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const VENDOR_LIBS = _.keys(require('./package.json').dependencies);
 const NODE_DIR = path.join(__dirname, 'node_modules');
-const BROKEN_SRC_MAPS = [
-  path.join(NODE_DIR, '@blueprintjs')
-];
 
 let config = {
   entry: {
@@ -26,7 +23,7 @@ let config = {
     loaders: [
       { test: /\.html$/, loader: 'html' },
       { test: /\.tsx?$/, loader: 'ts' },
-      { test: /node_modules.*\.js$/, loader: 'source-map-loader', exclude: BROKEN_SRC_MAPS },
+      { test: /node_modules.*\.js$/, loader: 'source-map-loader' },
       { test: /\.css$/, loader: 'style!css' },
       { test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.eot$/, loader: 'file' },
       { test: /\.less/, loader: 'style!css?sourceMap!less?sourceMap' }
