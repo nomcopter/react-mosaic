69977ae9abc26ca38a36f5aca9fb388029393d47
Alex Kreidler <alexkreidler2020@gmail.com>
2020-08-27 17:06:34 -0400

    make blueprint dependency explicit, export both themes

--- src/util/OptionalBlueprint.tsx
+++ src/util/OptionalBlueprint.tsx
@@ -1,38 +1,20 @@
-import { Classes as ClassesImport, IIconProps } from '@blueprintjs/core';
-import { IconNames as IconNamesTypeImport } from '@blueprintjs/icons';
+import { Classes as ClassesImport, Icon as IconImport } from '@blueprintjs/core';
+import { IconNames as IconNamesImport } from '@blueprintjs/icons';
 import * as React from 'react';
 
 export namespace OptionalBlueprint {
-  type Classes = typeof ClassesImport;
-  let Classes: Classes | undefined;
-  export let Icon: React.ReactType<IIconProps>;
-  type IconNames = typeof IconNamesTypeImport;
-  let IconNames: IconNames | undefined;
-  try {
-    // Webpack is quieter about these errors
-    // https://github.com/nomcopter/react-mosaic/issues/109
-    require.resolve('@blueprintjs/core');
-    require.resolve('@blueprintjs/icons');
-    ({ Classes, Icon } = require('@blueprintjs/core'));
-    ({ IconNames } = require('@blueprintjs/icons'));
-  } catch {
-    Icon = ({ icon }: IIconProps) => <span>{icon}</span>;
+  const Classes = ClassesImport;
+  const IconNames = IconNamesImport;
+
+  export let Icon = IconImport;
+
+  type ClassName = keyof typeof Classes;
+  export function getClasses(...names: ClassName[]): string {
+    return names.map((name) => Classes[name]).join(' ');
   }
 
-  type BlueprintClass = { [K in keyof Classes]: Classes[K] extends string ? K : never }[keyof Classes];
-  export function getClasses(...names: BlueprintClass[]): string {
-    if (Classes) {
-      return names.map((name) => Classes![name]).join(' ');
-    }
-
-    return '';
-  }
-
-  export function getIconClass(iconName: keyof IconNames): string {
-    if (Classes && IconNames) {
-      return Classes.iconClass(IconNames[iconName]);
-    }
-
-    return '';
+  type IconName = keyof typeof IconNamesImport;
+  export function getIconClass(iconName: IconName): string {
+    return Classes.iconClass(IconNames[iconName]);
   }
 }
