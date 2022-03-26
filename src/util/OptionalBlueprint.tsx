abce0f09b2ae791b049ea227d844b83b74f06b1d
Nicholas Paldino <casperOne@caspershouse.com>
2022-03-25 21:04:15 -0400

    Optional load of blueprintjs.

--- src/util/OptionalBlueprint.tsx
+++ src/util/OptionalBlueprint.tsx
@@ -4,8 +4,33 @@ import classNames from 'classnames';
 import _ from 'lodash';
 import * as React from 'react';
 
+// Performs a top level import of blueprint and gets the classes.
+async function getBluePrintClasses() {
+  // The classes.
+  let classes: Classes | undefined
+
+  // Wrap in a try/catch.
+  try
+  {
+    // Import the module.
+    const module = await import("@blueprintjs/core")
+
+    // If we're here, we have the module, get classes from  it.
+    classes = module.Classes
+  }
+  catch (ex) {
+    // Ignore for now.
+  }
+
+  // Return the classes.
+  return classes
+}
+
+// Get the classes.
+const BlueprintClasses: Classes | undefined = await getBluePrintClasses()
+
 export namespace OptionalBlueprint {
-  const BP_NAMESPACE = Classes['getClassNamespace']?.() ?? 'bp3';
+  const BP_NAMESPACE = BlueprintClasses?.getClassNamespace() ?? 'bp3';
   export const Icon = ({
     icon,
     className,
