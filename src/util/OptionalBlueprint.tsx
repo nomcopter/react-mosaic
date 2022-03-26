9ae5d31230bdf9851f47822fcfd1541c685b3dd5
Nicholas Paldino <casperOne@caspershouse.com>
2022-03-26 16:38:04 -0400

    Reverting to optional load from blueprint

--- src/util/OptionalBlueprint.tsx
+++ src/util/OptionalBlueprint.tsx
@@ -1,11 +1,39 @@
-import { Classes } from '@blueprintjs/core';
+import type { Classes } from '@blueprintjs/core';
 import type { IconNames } from '@blueprintjs/icons';
 import classNames from 'classnames';
 import _ from 'lodash';
 import * as React from 'react';
 
+// The fallback in case
+// the module doesn't exist or there is
+// some other error.
+const BLUEPRINT_CLASS_NAMESPACE_FALLBACK = 'bp3';
+
+// Performs a top level import of blueprint and gets the
+// namespace from the Classes export.
+async function getBluePrintClassNamespace() {
+  // Wrap in a try/catch.
+  try
+  {
+    // Import the module.
+    const module = await import("@blueprintjs/core")
+
+    // If we're here, we have the module, get Classes from  it
+    // and get the namespace.
+    return module.Classes.getClassNamespace()
+  }
+  catch (ex) {
+    // Ignore for now.
+  }
+
+  // Return the fallback.
+  return BLUEPRINT_CLASS_NAMESPACE_FALLBACK
+}
+
+// Get the blueprint class namespace.
+const BP_NAMESPACE = await getBluePrintClasses()
+
 export namespace OptionalBlueprint {
-  const BP_NAMESPACE = Classes.getClassNamespace();
   export const Icon = ({
     icon,
     className,
