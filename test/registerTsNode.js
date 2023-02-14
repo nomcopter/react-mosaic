a4dd544251d6a0fac2cec7adb08620d9730d5972
Kevin Verdieck <kevin@ironcladapp.com>
2023-02-13 18:14:32 -0800

    Downgrade mocha to a version before it knew about ESM...

--- test/registerTsNode.js
+++
@@ -1,4 +0,0 @@
-// It should still run the tests if there are TypeScript complaints, those problems will be handled by the build step.
-return require("ts-node").register({
-  disableWarnings: true,
-});
